480bd130b6c5e6d0922505aaed086060e788ee81
Greg Smith <crashingwaves@fea.st>
2015-10-09 10:51:11 -0600

    Pomodoro functionality complete

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,36 +1,52 @@
-function timerObj(duration){
+function timerObj(duration, isCurrent){
 	return{
 		duration: duration,
 		timer: duration * 60,
 		interval: 1000,
 		isRunning: false,
-		intervalHandle: null
+		intervalHandle: null,
+		isCurrentTimer: isCurrent || false,
 	};
 }
 
 function counterObj(){
 	return{
-		increment: function(timerObj){
+		incrementMinutes: function(timerObj){
 			timerObj.timer += 60;
 		},
-		decrement: function(timerObj){
+		decrementMinutes: function(timerObj){
 			timerObj.timer -= 60;
 		},
-		startTimer: function(timerObj){
-			if(!timerObj.isRunning){
+		incrementSeconds: function(timerObj){
+			timerObj.timer += 1;
+		},
+		decrementSeconds: function(timerObj){
+			timerObj.timer -= 1;
+		},
+		startTimer: function(timerObj, pendingTimer){
+			var _this = this;
+			if(!timerObj.isRunning && timerObj.isCurrentTimer){
 				timerObj.isRunning = true;
-				console.log(timerObj.timer);
-				timerObj.intervalHandle = setInterval(function(){
+				console.log(timerObj.timer); //debug
+				timerObj.intervalHandle = (function(){
+					return setInterval(function(){
 					if(timerObj.timer > 0){
 						timerObj.timer -= 1;
-						console.log(timerObj.timer);
+						console.log(timerObj.timer); //debug
 					} else {
 						clearInterval(timerObj.intervalHandle);
 						timerObj.isRunning = false;
-						console.log("Countdown complete");
+						console.log("Countdown complete"); //debug
+						if(pendingTimer){
+							_this.resetTimer(timerObj);
+							timerObj.isCurrentTimer = false;
+							pendingTimer.isCurrentTimer = true;
+							_this.startTimer(pendingTimer, timerObj);
+						}
 					}
 					
 				}, timerObj.interval);
+				})(_this)
 			} 
 		},
 		stopTimer: function(timerObj){
@@ -38,7 +54,7 @@ function counterObj(){
 				clearInterval(timerObj.intervalHandle);
 				timerObj.isRunning = false;
 			}else{
-				console.log("Timer isn't running!");
+				console.log("Timer isn't running!");//debug
 				return;
 			}
 		},
@@ -54,11 +70,33 @@ function counterObj(){
 	}
 }
 
+
 function pomodoro(){
-	return {
-		workTimer: timerObj(25),
-		breakTimer: timerObj(5),
-		counterController: counterObj()
+	return{
+		workTimer: timerObj(.1, true),
+		breakTimer: timerObj(.05),
+		counterController: counterObj(),
+		currentTimer: this.workTimer,
+		pendingTimer: this.breakTimer,
+		checkCurrent: function(){
+			if(this.workTimer.isCurrentTimer){
+				this.currentTimer = this.workTimer;
+				this.pendingTimer = this.breakTimer;
+			} else {
+				this.currentTimer = this.breakTimer;
+				this.pendingTimer = this.workTimer;
+			}
+		},
+		toggleTimer: function(){
+			this.checkCurrent();
+			if(this.currentTimer.isRunning){
+				this.counterController.stopTimer(this.currentTimer);
+			} else {
+				this.counterController.startTimer(this.currentTimer, this.pendingTimer);
+			}
+		},
+		
 	}
 }
 
+var catchup = pomodoro();
